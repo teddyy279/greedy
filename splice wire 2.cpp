49a70278f140bae69e018a8818139928d9055416
teddy279 <phambaokhue2792004@gmail.com>
2024-03-21 00:15:20 +0700

    upload source code
    vv

---
+++ splice wire 2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    int a[n];
+    priority_queue<int> pq;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        pq.push(a[i]);
+    }
+    int res = 0;
+    while(pq.size() >= 2){
+        int t1 = pq.top(); pq.pop();
+        int t2 = pq.top(); pq.pop();
+        res += t1 + t2;
+        pq.push(t1 + t2);
+    }
+    cout << res;
+}
